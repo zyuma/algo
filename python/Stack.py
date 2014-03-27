5effd960f6633757532fe6e5a87cc8f837d014d1
zyuma <yuma.tsuboi@gmail.com>
2014-03-27 00:25:28 -0700

    Stack that can get smallest in O(1)

--- python/Stack.py
+++ python/Stack.py
@@ -2,18 +2,23 @@ class Node:
 	def __init__(self, x):
 		self.val = x
 		self.next = None
+		self.smallest = self
 	
 class Stack:
 	def __init__(self):
 		self.top = None
 	def push(self, x):
+		if self.top != None and x.val > self.top.smallest.val:
+			x.smallest = self.top.smallest
 		x.next = self.top
-		self.top = x
-	
+		self.top = x	
 	def pop(self):
 		temp = self.top
 		self.top = self.top.next
 		return temp
+	def smallest(self):
+		return self.top.smallest.val
+
 	def print_stack(self):
 		curr = self.top
 		while curr != None:
@@ -21,24 +26,28 @@ class Stack:
 			curr = curr.next
 
 
-n1 = Node(1)
-n2 = Node(2)
-n3 = Node(3)
-n4 = Node(4)
-n5 = Node(5)
-n6 = Node(6)
-n7 = Node(7)
+n1 = Node(7)
+n2 = Node(4)
+n3 = Node(5)
+n4 = Node(6)
+n5 = Node(3)
+n6 = Node(2)
+n7 = Node(1)
 
 s = Stack();
 s.push(n1)
+print s.smallest()
 s.push(n2)
+print s.smallest()
 s.push(n3)
-print "bfore"
-s.print_stack()
-s.pop()
+print s.smallest()
 s.push(n4)
+print s.smallest()
 s.push(n5)
+print s.smallest()
 s.push(n6)
+print s.smallest()
 s.push(n7)
+print s.smallest()
 print "after"
 s.print_stack()
